eaa07bf1dcbe76233f53a416dcdc46099da8c589
Florian Chartier <fwchartier@gmail.com>
2023-09-16 22:55:53 +0200

    improve event listener for number of player, works well now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
+// to listen and store players'number
 let numberOfPlayer = document.getElementById('number-of-player');
 
 numberOfPlayer.addEventListener("change", function () {
-    numberOfPlayer.value;
-    console.log(numberOfPlayer.value)
     switch (numberOfPlayer.value) {
+        case '1':
+            document.getElementById('second-player-name').classList.remove('active');
+            document.getElementById('third-player-name').classList.remove('active');
+            document.getElementById('fourth-player-name').classList.remove('active');
+            break;
         case '2':
             document.getElementById('second-player-name').classList.add('active');
+            document.getElementById('third-player-name').classList.remove('active');
+            document.getElementById('fourth-player-name').classList.remove('active');
             break;
         case '3':
+            document.getElementById('second-player-name').classList.add('active');
             document.getElementById('third-player-name').classList.add('active');
+            document.getElementById('fourth-player-name').classList.remove('active');
             break;
         case '4':
+            document.getElementById('second-player-name').classList.add('active');
+            document.getElementById('third-player-name').classList.add('active');
             document.getElementById('fourth-player-name').classList.add('active');
             break;
     }
 });
 
-
+// to record name, score, time of ten best players
 let scores = JSON.parse(localStorage.getItem('bestScores')) || [];
 let gameOverTime;
 let gamerName;

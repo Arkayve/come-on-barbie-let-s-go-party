0124fc43012756fb63b9acd0acc3ad33cfc01646
Florian Chartier <fwchartier@gmail.com>
2023-09-16 21:52:27 +0200

    base of html, multiscore local storage and getName function

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,26 @@
 // const urlParams = new URLSearchParams(window.location.search);
 // let code = urlParams.get('code');
 
+let scores = JSON.parse(localStorage.getItem('bestScores')) || [];
+let gameOverTime;
+let gamerName;
+let score;
+
+function getName(id) {
+    gamerName = document.getElementById(id).value;
+    if (gamerName.trim() !== "") savePlayerInformation();
+}
+
+function savePlayerInformation() {
+    let party = {};
+    party["name"] = gamerName;
+    party["score"] = score;
+    party["time"] = gameOverTime;
+    scores.push(party);
+    scores.sort((a, b) => b.score - a.score);
+    const maxScoresToKeep = 10;
+    scores = scores.slice(0, maxScoresToKeep);
+    localStorage.setItem('bestScores', JSON.stringify(scores));
+    console.log(scores);
+}
+

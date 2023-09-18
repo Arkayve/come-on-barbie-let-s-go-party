ee5c5702e6a6f5031d25da2476ea3d8dcc6f0d6e
Florian <fwchartier@gmail.com>
2023-09-18 22:52:17 +0200

    playback don't work due to device id problem, but many other things work

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,12 @@
 // Les paramètres de la demande POST pour obtenir le jeton d'accès
-const clientId = '8bd47bc26ab94bb881b0128f77d2ef5b';
-const clientSecret = 'ecbfa94f20fc4523920ca9e41d057f2d';
+const clientId = localStorage.getItem('spotifyClientId');
+const clientSecret = localStorage.getItem('spotifyClientSecret');
+const scope = 'user-modify-playback-state streaming';
 const postData = new URLSearchParams({
   grant_type: 'client_credentials',
   client_id: clientId,
-  client_secret: clientSecret
+  client_secret: clientSecret,
+  scope: scope
 });
 
 // Options de la requête
@@ -15,6 +17,7 @@ let requestOptions = {
   },
   body: postData
 };
+console.log(requestOptions);
 
 // URL de la requête
 let url = 'https://accounts.spotify.com/api/token';
@@ -23,6 +26,7 @@ let url = 'https://accounts.spotify.com/api/token';
 fetch(url, requestOptions)
   .then(response => response.json())
   .then(data => {
+    console.log(data);
     // Stockez la réponse dans le localStorage ou utilisez-la comme nécessaire
     localStorage.setItem('spotifyAccessToken', data.access_token);
   })
@@ -30,7 +34,6 @@ fetch(url, requestOptions)
     console.error('Erreur lors de la requête POST :', error);
   });
 
-  
 
 
 
@@ -39,7 +42,8 @@ fetch(url, requestOptions)
 
 
 
-  // Remplacez cette valeur par votre jeton d'accès Spotify
+
+// Remplacez cette valeur par votre jeton d'accès Spotify
 const accessToken = localStorage.getItem('spotifyAccessToken');
 
 // URL de la requête GET
@@ -71,11 +75,11 @@ fetch(url_2, requestOptions_2)
 
 
 
-  window.onSpotifyWebPlaybackSDKReady = () => {
-    initializeSpotifyPlayer();
-  };
-  
-  function initializeSpotifyPlayer() {
+window.onSpotifyWebPlaybackSDKReady = () => {
+  initializeSpotifyPlayer();
+};
+
+function initializeSpotifyPlayer() {
 
 
   const player = new Spotify.Player({
@@ -84,7 +88,7 @@ fetch(url_2, requestOptions_2)
       callback(accessToken);
     }
   });
-  
+
   // Add listeners for player events (e.g., "ready", "player_state_changed")
   player.addListener('ready', ({ device_id }) => {
     console.log('device_id : ', device_id);
@@ -97,7 +101,12 @@ fetch(url_2, requestOptions_2)
       console.error('Device ID is undefined. Player may not be connected properly.');
     }
   });
-  
+
+  // Add error listener to handle any player errors
+  player.addListener('initialization_error', ({ message }) => {
+    console.error('Spotify player initialization error:', message);
+  });
+
   // Connect to the Spotify player
   player.connect().then(success => {
     if (success) {
@@ -107,18 +116,23 @@ fetch(url_2, requestOptions_2)
     }
   });
 }
-  
-  function playTrack(device_id) {
-    fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
-      method: 'PUT',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        uris: ['spotify:track:4OROzZUy6gOWN4UGQVaZMF'] // Replace with the URI of the track you want to play
-      })
+
+function playTrack(device_id) {
+  if (!device_id) {
+    console.error('Device ID is undefined. Make sure the device is properly connected.');
+    return;
+  }
+
+  fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      uris: ['spotify:track:4OROzZUy6gOWN4UGQVaZMF'] // Replace with the URI of the track you want to play
     })
+  })
     .then(response => {
       if (response.ok) {
         console.log('Track started playing successfully.');
@@ -129,35 +143,4 @@ fetch(url_2, requestOptions_2)
     .catch(error => {
       console.error('Error starting track:', error);
     });
-  }
-
-
-
-
-
-
-
-
-
-// URL de la requête GET
-let url_3 = 'https://api.spotify.com/v1/me/player/devices';
-
-// Options de la requête avec l'en-tête d'autorisation
-let requestOptions_3 = {
-  method: 'GET',
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-};
-
-// Effectuez la requête GET pour obtenir la liste des appareils connectés
-fetch(url_3, requestOptions_3)
-  .then(response => response.json())
-  .then(data => {
-    // Utilisez les données de la réponse comme nécessaire
-    console.log(data);
-    console.log('Liste des appareils connectés :', data.devices);
-  })
-  .catch(error => {
-    console.error('Erreur lors de la requête GET pour les appareils :', error);
-  });
+}

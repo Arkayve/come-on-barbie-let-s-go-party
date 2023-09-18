44655b2f608ab1f079c6780dfc06b213626df91b
Florian Chartier <fwchartier@gmail.com>
2023-09-18 16:50:05 +0200

    trying to set a player from spotify api

--- assets/js/api.js
+++ assets/js/api.js
@@ -43,10 +43,10 @@ fetch(url, requestOptions)
 const accessToken = localStorage.getItem('spotifyAccessToken');
 
 // URL de la requête GET
-url = 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao';
+let url_2 = 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao';
 
 // Options de la requête avec l'en-tête d'autorisation
-requestOptions = {
+let requestOptions_2 = {
   method: 'GET',
   headers: {
     'Authorization': `Bearer ${accessToken}`
@@ -54,7 +54,7 @@ requestOptions = {
 };
 
 // Effectuez la requête GET
-fetch(url, requestOptions)
+fetch(url_2, requestOptions_2)
   .then(response => response.json())
   .then(data => {
     // Utilisez les données de la réponse comme nécessaire
@@ -62,4 +62,102 @@ fetch(url, requestOptions)
   })
   .catch(error => {
     console.error('Erreur lors de la requête GET :', error);
+  });
+
+
+
+
+
+
+
+
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    initializeSpotifyPlayer();
+  };
+  
+  function initializeSpotifyPlayer() {
+
+
+  const player = new Spotify.Player({
+    name: 'My Web Player',
+    getOAuthToken: callback => {
+      callback(accessToken);
+    }
+  });
+  
+  // Add listeners for player events (e.g., "ready", "player_state_changed")
+  player.addListener('ready', ({ device_id }) => {
+    console.log('device_id : ', device_id);
+    if (device_id) {
+      console.log('Spotify player is ready with device ID', device_id);
+
+      // Play a Spotify track when the player is ready
+      playTrack(device_id);
+    } else {
+      console.error('Device ID is undefined. Player may not be connected properly.');
+    }
+  });
+  
+  // Connect to the Spotify player
+  player.connect().then(success => {
+    if (success) {
+      console.log('Connected to Spotify player');
+    } else {
+      console.error('Failed to connect to Spotify player');
+    }
+  });
+}
+  
+  function playTrack(device_id) {
+    fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        uris: ['spotify:track:4OROzZUy6gOWN4UGQVaZMF'] // Replace with the URI of the track you want to play
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Track started playing successfully.');
+      } else {
+        console.error('Error starting track:', response.status);
+      }
+    })
+    .catch(error => {
+      console.error('Error starting track:', error);
+    });
+  }
+
+
+
+
+
+
+
+
+
+// URL de la requête GET
+let url_3 = 'https://api.spotify.com/v1/me/player/devices';
+
+// Options de la requête avec l'en-tête d'autorisation
+let requestOptions_3 = {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${accessToken}`
+  }
+};
+
+// Effectuez la requête GET pour obtenir la liste des appareils connectés
+fetch(url_3, requestOptions_3)
+  .then(response => response.json())
+  .then(data => {
+    // Utilisez les données de la réponse comme nécessaire
+    console.log(data);
+    console.log('Liste des appareils connectés :', data.devices);
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête GET pour les appareils :', error);
   });

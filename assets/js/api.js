37cf368d95950bf989ba86b44fc46945ce0861bd
Florian <fwchartier@gmail.com>
2023-09-21 16:12:05 +0200

    make many changes to try to connect like i need

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,151 +1,117 @@
-// Les paramètres de la demande POST pour obtenir le jeton d'accès
-const clientId = localStorage.getItem('spotifyClientId');
-const clientSecret = localStorage.getItem('spotifyClientSecret');
-const scope = 'user-modify-playback-state streaming';
-const postData = new URLSearchParams({
-  grant_type: 'client_credentials',
-  client_id: clientId,
-  client_secret: clientSecret,
-  scope: scope
+// The code verifier will be generated using the following JavaScript function:
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+  
+//   Once the code verifier has been generated, we must transform (hash) it using the SHA256 algorithm. This is the value that will be sent within the user authorization request.
+
+// The generateCodeChallenge function returns the base64 representation of the digest by calling to base64encode():
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+  }
+
+//   The code to request the user authorization looks like this:
+// const clientId = localStorage.getItem('spotifyClientId');
+const clientId = '8bd47bc26ab94bb881b0128f77d2ef5b';
+const redirectUri = 'http://localhost:5500';
+
+let codeVerifier = generateRandomString(128);
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
 });
 
-// Options de la requête
-let requestOptions = {
+// We must parse the URL and save the code parameter to request the access token afterwards:
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+
+// The body of the request can be implemented as follows:
+// let codeVerifier = localStorage.getItem('code_verifier');
+// ERROR : Uncaught SyntaxError: Identifier 'codeVerifier' has already been declared (at api2.js:64:5)
+
+let body = new URLSearchParams({
+  grant_type: 'authorization_code',
+  code: code,
+  redirect_uri: redirectUri,
+  client_id: clientId,
+  code_verifier: codeVerifier
+});
+
+// Finally, we can make the POST request and store the access token by parsing the JSON response from the server:
+const response = fetch('https://accounts.spotify.com/api/token', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   },
-  body: postData
-};
-console.log(requestOptions);
-
-// URL de la requête
-let url = 'https://accounts.spotify.com/api/token';
-
-// Effectuez la requête POST
-fetch(url, requestOptions)
-  .then(response => response.json())
+  body: body
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('HTTP status ' + response.status);
+    }
+    console.log(response.json());
+    return response.json();
+  })
   .then(data => {
-    console.log(data);
-    // Stockez la réponse dans le localStorage ou utilisez-la comme nécessaire
-    localStorage.setItem('spotifyAccessToken', data.access_token);
+    localStorage.setItem('access_token', data.access_token);
   })
   .catch(error => {
-    console.error('Erreur lors de la requête POST :', error);
+    console.error('Error:', error);
   });
 
-
-
-
-
-
-
-
-
-
-// Remplacez cette valeur par votre jeton d'accès Spotify
-const accessToken = localStorage.getItem('spotifyAccessToken');
-
-// URL de la requête GET
-let url_2 = 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao';
-
-// Options de la requête avec l'en-tête d'autorisation
-let requestOptions_2 = {
-  method: 'GET',
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-};
-
-// Effectuez la requête GET
-fetch(url_2, requestOptions_2)
-  .then(response => response.json())
-  .then(data => {
-    // Utilisez les données de la réponse comme nécessaire
-    console.log('Données de l\'artiste Spotify:', data);
-  })
-  .catch(error => {
-    console.error('Erreur lors de la requête GET :', error);
-  });
-
-
-
-
-
-
-
-
-window.onSpotifyWebPlaybackSDKReady = () => {
-  initializeSpotifyPlayer();
-};
-
-function initializeSpotifyPlayer() {
-
-
-  const player = new Spotify.Player({
-    name: 'My Web Player',
-    getOAuthToken: callback => {
-      callback(accessToken);
-    }
-  });
-
-  // Add listeners for player events (e.g., "ready", "player_state_changed")
-  player.addListener('ready', ({ device_id }) => {
-    console.log('device_id : ', device_id);
-    if (device_id) {
-      console.log('Spotify player is ready with device ID', device_id);
-
-      // Play a Spotify track when the player is ready
-      playTrack(device_id);
-    } else {
-      console.error('Device ID is undefined. Player may not be connected properly.');
-    }
-  });
-
-  // Add error listener to handle any player errors
-  player.addListener('initialization_error', ({ message }) => {
-    console.error('Spotify player initialization error:', message);
-  });
-
-  // Connect to the Spotify player
-  player.connect().then(success => {
-    if (success) {
-      console.log('Connected to Spotify player');
-    } else {
-      console.error('Failed to connect to Spotify player');
-    }
-  });
-}
-
-function playTrack(device_id) {
-  if (!device_id) {
-    console.error('Device ID is undefined. Make sure the device is properly connected.');
-    return;
-  }
-
-  document.getElementById('play-track').addEventListener('click', function() {
-    console.log('click');
-    playTrack(device_id);
-  })
-
-  fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      uris: ['spotify:track:4OROzZUy6gOWN4UGQVaZMF'] // Replace with the URI of the track you want to play
-    })
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log('Track started playing successfully.');
-      } else {
-        console.error('Error starting track:', response.status);
+//   The following code implements the getProfile() function which performs the API call to the /me endpoint in order to retrieve the user profile related information:
+async function getProfile(accessToken) {
+    // let accessToken = localStorage.getItem('access_token');
+    // ERROR : Uncaught SyntaxError: Identifier 'accessToken' has already been declared (at api2.js:98:9)
+    accessToken = localStorage.getItem('access_token');
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
       }
-    })
-    .catch(error => {
-      console.error('Error starting track:', error);
     });
-}
+  
+    const data = await response.json();
+    console.log(data);
+  }
+
+
+//   In order to refresh the token, a POST request must be sent with the following body parameters encoded in application/x-www-form-urlencoded:
+
+// REQUEST BODY PARAMETER --VALUE
+// grant_type --Required Set it to refresh_token.
+// refresh_token --Required The refresh token returned from the authorization code exchange.
+// client_id --Required The client ID for your app, available from the developer dashboard.
+// The headers of this POST request must contain the Content-Type header set to application/x-www-form-urlencoded value.

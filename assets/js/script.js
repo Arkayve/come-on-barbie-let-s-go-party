a6592dcafc473519b083bd9d223a6866d360c340
Florian <fwchartier@gmail.com>
2023-10-01 22:27:20 +0200

    bas of html for own-quiz. have to design that functionnality before continue

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,7 +124,7 @@ function displayBestScores(where) {
         });
     } else {
         const listItem = document.createElement('li');
-        listItem.textContent = 'No scores recorded.';
+        listItem.textContent = 'No score recorded.';
         where.appendChild(listItem);
     }
 }
@@ -437,4 +437,5 @@ document.getElementById('home-mushroom').addEventListener('click', function (eve
 })
 
 // backup of bestScores for test of localstorage functions in another computer
-// [{"name":"A","score":500},{"name":"B","score":450},{"name":"C","score":400},{"name":"D","score":350},{"name":"E","score":300},{"name":"F","score":250},{"name":"G","score":200},{"name":"H","score":150},{"name":"I","score":100},{"name":"J","score":50}]
+// [{"name":"A","score":500},{"name":"B","score":450},{"name":"C","score":400},{"name":"D","score":350},{"name":"E","score":300},{"name":"F","score":250},{"name":"G","score":200},{"name":"H","score":150},{"name":"I","score":100},{"name":"J","score":50}];
+

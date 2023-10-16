e04b407d3428fa3642c99ec5ae865e4735d6daf7
Florian <fwchartier@gmail.com>
2023-10-16 22:00:58 +0200

    make config.php file

---
+++ assets/includes/_config.php
@@ -0,0 +1,64 @@
+<?php
+
+$quiz = [
+    [
+        'name' => 'American Series',
+        'file' => 'american_series.json',
+    ],
+    [
+        'name' => 'Belgian Beers',
+        'file' => 'belgian_beers.json',
+    ],
+    [
+        'name' => 'Bulk Cultivation',
+        'file' => 'bulk_cultivation.json',
+    ],
+    [
+        'name' => 'Cartoons',
+        'file' => 'cartoons.json',
+    ],
+    [
+        'name' => 'Company Facts',
+        'file' => 'company_facts.json',
+    ],
+    [
+        'name' => 'COVID-19',
+        'file' => 'covid_19.json',
+    ],
+    [
+        'name' => 'Great Monuments',
+        'file' => 'great_monuments.json',
+    ],
+    [
+        'name' => 'Heroes of Kaamelott',
+        'file' => 'heroes_of_kaamelott.json',
+    ],
+    [
+        'name' => 'Internet Statistics 2018',
+        'file' => 'internet_statistics_2018.json',
+    ],
+    [
+        'name' => 'Mont Saint-Michel',
+        'file' => 'mont_saint_michel.json',
+    ],
+    [
+        'name' => 'Myths and Legends',
+        'file' => 'myths_and_legends.json',
+    ],
+    [
+        'name' => 'Paris Today',
+        'file' => 'paris_today.json',
+    ],
+    [
+        'name' => 'StarTrek',
+        'file' => 'startrek.json',
+    ],
+    [
+        'name' => 'Trademarks and Slogans',
+        'file' => 'trademarks_and_slogans.json',
+    ],
+    [
+        'name' => 'X-Files',
+        'file' => 'x_files.json',
+    ]
+];
